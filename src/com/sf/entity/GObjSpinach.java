46b0da9500e9b082222b2388fa2f84bfed5ce255
canyons <gongyanghui1986@163.com>
2018-12-01 21:11:10 +0800

    菠菜逻辑？

--- src/com/sf/entity/GObjSpinach.java
+++ src/com/sf/entity/GObjSpinach.java
@@ -11,9 +11,6 @@ import com.bowlong.lang.RndEx;
 public class GObjSpinach extends GObject {
 	private static final long serialVersionUID = 1L;
 
-	private double disPos; // 位置
-	private long nextLiveMs; // 下次复活时间
-
 	public GObjSpinach() {
 		super(ETGObj.Spinach, 0);
 	}
@@ -21,45 +18,28 @@ public class GObjSpinach extends GObject {
 	public void startRnd(long beTo) {
 		if (now() <= this.nextLiveMs)
 			return;
-		this.nextLiveMs = 0;
-		this.disPos = RndEx.nextDouble(GObjConfig.NMin_PosSpinach, GObjConfig.NMax_PosSpinach);
 		setBelongTo(beTo); // 通过beto来判断距离(disPos 的距离)
 		setRunTo(0);
-		setRunway(rndWay());
-		this.nState = 1;
-		// setRunning(true);
+		setRunway(0);
+		double _initPos = RndEx.nextDouble(GObjConfig.NMin_PosSpinach, GObjConfig.NMax_PosSpinach);
+		ready(_initPos);
 	}
 
 	public double diffDistance(GObject gobj) {
 		double lens = GObjConfig.LenMax_Runway;
-		if(gobj == null)
+		if (gobj == null)
 			return lens;
-		
+
 		double dis = gobj.calcDistance();
+		double disCur = calcDistance();
 		boolean isSameDir = gobj.getId() == getBelongTo();
-		return isSameDir ? (this.disPos - dis) : (lens - this.disPos - dis);
+		return isSameDir ? (disCur - dis) : (lens - disCur - dis);
 	}
 
 	public void disappear(boolean isReLive) {
-		stop();
+		super.disappear(isReLive);
 		setBelongTo(0);
-		this.disPos = 0;
 		if (isReLive)
 			this.nextLiveMs = now() + GObjConfig.LMS_NextLive_Spinach;
-		else
-			this.nextLiveMs = 0;
 	}
-	
-	public boolean isCanRelive() {
-		if (this.nextLiveMs > 0)
-			return this.nextLiveMs <= now();
-		return false;
-	}
-
-	@Override
-	public double calcDistance() {
-		return this.disPos;
-	}
-	
-	
 }
